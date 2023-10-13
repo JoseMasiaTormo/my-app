ba452cbee0ae33a7224279203e2c3e339a0f0e18
JoseMasiaTormo <jotatech1441@gmail.com>
2023-10-13 10:18:23 +0200

    Mi aplicacion con crud

---
+++ src/Controller/LibroController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Libro;
+
+class LibroController extends AbstractController
+{
+    private $libros = [
+        1 => ["nombre" => "Indomable", "autor" => "David Tesch", "año" => "2022"],
+        2 => ["nombre" => "El infierno", "autor" => "Carmen Mola", "año" => "2023"],
+        5 => ["nombre" => "Mirafiori", "autor" => "Manuel Jabois", "año" => "2023"],
+        7 => ["nombre" => "La armadura de la luz", "autor" => "Ken Follet", "año" => "2023"],
+        9 => ["nombre" => "Éxtasis", "autor" => "Tracy Wolff", "año" => "2023"]
+    ];     
+
+    #[Route('/libro/insertar/{nombre}/{autor}/{año}', name:'insertar_libro')]
+    public function insertar(ManagerRegistry $doctrine, $nombre, $autor, $año) {
+        $entityManager = $doctrine->getManager();
+        $libro = new Libro();
+        $libro->setNombre($nombre);
+        $libro->setAutor($autor);
+        $libro->setAño($año);
+        $entityManager->persist($libro);
+
+        try {
+            $entityManager->flush();
+            return new Response("Libro insertado");
+        } catch (\Exception $e) {
+            return new Response("Error insertando objetos");
+        }
+    }
+
+    #[Route('/libro/update/{id}/{nombre}', name: 'modificar_libro')]
+    public function update(ManagerRegistry $doctrine, $id, $nombre): Response {
+        $entityManager = $doctrine->getManager();
+        $repositorio = $doctrine->getRepository(Libro::class);
+        $libro = $repositorio->find($id);
+        if ($libro) {
+            $libro->setNombre($nombre);
+            try {
+                $entityManager->flush();
+                return $this->render('ficha_libro.html.twig', [
+                    'libro' => $libro
+                ]);
+            } catch (\Exception $e) {
+                return new Response("Error insertando objetos");
+            }
+        } else {
+            return $this->render('ficha_libro.html.twig', [
+                'libro' => null
+            ]);
+        }
+    }
+
+    #[Route('/libro/delete/{id}', name: 'borrar_libro')]
+    public function delete(ManagerRegistry $doctrine, $id): Response{
+        $entityManager = $doctrine->getManager();
+        $repositorio = $doctrine->getRepository(Libro::class);
+        $libro = $repositorio->find($id);
+
+        if ($libro) {
+            try {
+                $entityManager->remove($libro);
+                $entityManager->flush();
+                return new Response("Libro eliminado");
+            } catch (\Exception $e) {
+                return new Response("Error eliminando objeto");
+            }
+        } else {
+            return $this->render('ficha_libro.html.twig', [
+                'libro' => null
+            ]);
+        }
+    }
+
+    #[Route('/libro/{codigo}', name: 'ficha_libro')]
+    public function index(ManagerRegistry $doctrine, $codigo): Response
+    {
+        $repositorio = $doctrine->getRepository(Libro::class);
+        $libro = $repositorio->find($codigo);
+
+        return $this->render('ficha_libro.html.twig', [
+        'libro' => $libro
+        ]);
+    }
+
+    #[Route('/libro/buscar/{texto}', name: 'buscar_libro')]
+    public function buscar(ManagerRegistry $doctrine, $texto):Response {
+        $repositorio = $doctrine->getRepository(Libro::class);
+        $libros = $repositorio->findByName($texto);
+
+        return $this->render('lista_libros.html.twig', [
+            'libros' => $libros
+        ]);
+    }
+}
---
+++ src/Entity/Libro.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LibroRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LibroRepository::class)]
+class Libro
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $autor = null;
+
+    #[ORM\Column]
+    private ?int $año = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getAutor(): ?string
+    {
+        return $this->autor;
+    }
+
+    public function setAutor(string $autor): static
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+
+    public function getAño(): ?int
+    {
+        return $this->año;
+    }
+
+    public function setAño(int $año): static
+    {
+        $this->año = $año;
+
+        return $this;
+    }
+}
